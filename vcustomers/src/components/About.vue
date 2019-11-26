3a54019f9d272ee20428cde291a1d7565ef5e800
amy923 <2420859817@qq.com>
2019-11-26 20:20:49 +0800

    first commit

---
+++ vcustomers/src/components/About.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="about container">
+    <h1 class="page-header">公司介绍</h1>
+    <div>我们公司成立于2008年，是一家专注于条码识别解决方案的创新型科技企业。<br/>
+      简洁、极致是我们遵循的原则，从用户需求、场景体验、产品定义、研发技术、工程测试、到企业运营管理。每一项工作，我们都追求在自己的领域，不断深挖、提升、追求不凡，直至最后走到极致。<br/>
+我们是一群不向现实妥协的人，追寻内心的梦想，享受工作的艰辛，也享受工作带来的成就感，我们一直在追寻，5年、10年…,   直至巅峰。 <br/>
+业务范围：一维、二维条码算法授权，一维、二维扫描引擎及模组定制，扫描引擎、模组、扫描枪、扫描平台成品ODM。 <br/>企业文化：简洁极致。<br/>
+      品牌定位：忠于不凡<br/>
+      产品理念：以用户体验为核心，打磨时尚科技精品。</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'about',
+  data () {
+    return {
+
+   } 
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
+
